ace4adcc44327f864867718c160580e271210817
Catie Collins <catherinetdcollins@gmail.com>
2023-09-26 04:00:57 -0500

    first commit

--- number_range_analysis.py
+++ number_range_analysis.py
@@ -13,7 +13,16 @@ Implement Number Analysis Functions
 # TODO: IN A COMMENT WITHIN EACH DEF, WRITE PSEUDOCODE FOR EACH SOLUTION
 
 def calculate_sum(start, end):
-    """
+    #Followig the return statement use you will want to use the sum() function, with the range() function inside of the sum() parenthesis. In the range() parethesis you will put start, end and + 1(to be inclusive)
+    return sum(range(start, end + 1))
+
+# You will want to assign the function name(calculate_sum) to a variable so that you can print out the result. The calculate_sum() function should have 2 numbers in the argument(within the parenthesis)
+get_sum = calculate_sum(0, 25)
+
+# The final step will be printing your calculated sum,by means of the variable you made
+print(get_sum)
+
+"""
     Calculate the sum of numbers within the specified range.
 
     Arguments:
@@ -23,11 +32,19 @@ def calculate_sum(start, end):
     Return:
         int: The sum of numbers within the range.
     """
+
     # TODO: Implement the logic to calculate the sum of numbers within the range.
     # TODO: Return the calculated sum.
-
+#===============================================================================
 def find_smallest_number(start, end):
-    """
+    # Followig the return statement use the min() function with range inside the parenthesis. Inside the range parenthesis you will put start, end + 1(to make it inclusive)
+    return min(range(start, end + 1))
+
+# You will want to assign the function name(smallest_num) to a variable so that you can print out the result. The find_smallest_num() function should have 2 numbers in the argument(within the parenthesis)
+smallest_number = find_smallest_number(0, 10)
+print(smallest_number)
+
+"""
     Find the smallest number within the specified range.
 
     Arguments:
@@ -40,8 +57,15 @@ def find_smallest_number(start, end):
     # TODO: Implement the logic to find the smallest number within the range.
     # TODO: Return the found smallest number.
 
+#===============================================================================
 def find_largest_number(start, end):
-    """
+        # Followig the return statement use the max() function with range inside the parenthesis. Inside the range parenthesis you will put start, end + 1(to make it inclusive)
+    return max(range(start, end + 1))
+
+# You will want to assign the function name(find_largest_number) to a variable so that you can print out the result. The find_largest_number() function should have 2 numbers in the argument(within the parenthesis)
+largest_number = find_largest_number(0, 15)
+print(largest_number)
+"""
     Find the largest number within the specified range.
 
     Args:
@@ -54,8 +78,21 @@ def find_largest_number(start, end):
     # TODO: Implement the logic to find the largest number within the range.
     # TODO: Return the found largest number.
 
+
 def count_even_numbers(start, end):
-    """
+    # Assign 0 to a variable, for the count of your function
+    count_num = 0
+    
+    # Start a for loop to iterate through the numbers of the range. You'll put inside the range() parenthesis start, end + 1. To check if the nuber is odd you use the modulo operator(%) and see if it has a remainder of 2, by not equaling to 0.  If the number is odd, it will increment the number assigned to the variable made at the beginning (count_num). Return that variable.
+    for i in range(start, end + 1):
+        if i % 2 == 0:
+            count_num += 1
+    return count_num
+
+# You will want to assign the function name(count_even_numbers) to a variable so that you can print out the result. The count_even_numbers() function should have 2 numbers in the argument(within the parenthesis)
+even_numbers = count_even_numbers(0, 30)
+print(even_numbers)
+"""
     Count the number of even numbers within the specified range.
 
     Args:
@@ -69,7 +106,19 @@ def count_even_numbers(start, end):
     # TODO: Return the count of even numbers.
 
 def count_odd_numbers(start, end):
-    """
+    # Assign 0 to a variable
+    count_odd = 0
+    
+    # Start a for loop to iterate through the numbers of the range. You'll put inside the range() parenthesis start, end + 1. To check if the nuber is odd you use the modulo operator(%) and see if it has a remainder of 2, by not equaling to 0.  If the number is odd, it will increment the number assigned to the variable made at the beginning (count_num). Return that variable.
+    for i in range(start, end + 1):
+        if i % 2 != 0:
+            count_odd += 1
+    return count_odd
+    
+# You will want to assign the function name(count_odd_numbers) to a variable so that you can print out the result. The count_odd_numbers() function should have 2 numbers in the argument(within the parenthesis)
+odd_numbers = count_odd_numbers(0, 5)
+print(odd_numbers)
+"""
     Count the number of odd numbers within the specified range.
 
     Args:
